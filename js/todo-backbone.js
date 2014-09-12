61f865b784165ce0625da5f997e913125ac684e2
Bryan Potter <potterbm@gmail.com>
2014-09-12 15:51:11 -0600

    First draft

--- js/todo-backbone.js
+++ js/todo-backbone.js
@@ -2,37 +2,85 @@
 var app = {};
 
 // Models
-app.Item = Backbone.Model.extend({
+app.TodoItem = Backbone.Model.extend({
 	defaults : {
 		text : '',
 		completed : false
 	},
-	
-	localStorage : new Store("todo-item"),
 	
 	complete : function() {
 		this.completed = true;
 	}
 });
 
-app.List = Backbone.Collection.extend({
-	model : Item,
-	url : "../api/lists"
+
+app.TodoList = Backbone.Collection.extend({
+	model : app.TodoItem,
+	
+	localStorage : new Store("todo-list")
 });
 
+app.todoList = new app.TodoList();
 
 
 
 // Views
 
-app.AppView = Backbone.View.extend({
+
+ap.TodoItemView = Backbone.View.extend({
+	
+	tagName : "li",
+	
+	className : "list-item",
+	
+	template : _.template($("#todo-item-template").html()),
+	
+	render : function() {
+		this.$el.html(this.template(this.model.toJSON()));
+		return this;
+	}
+	
+});
+
+
+app.ListView = Backbone.View.extend({
 	el : "#list",
 	
 	initialize : function() {
-		this.render();
+		this.input = this.$("#new-item");
+		
+		// Event Bindings
+		app.todoList.on("add", this.addOne, this);
+		app.todoList.on("reset", this.reset, this);
+		
+		app.todoList.fetch();
+	},
+	
+	events : {
+		'keypress #new-item' : 'createTodoOnEnter'
+	},
+	
+	createTodoOnEnter : function(e) {
+		if(e.which !== 13 || !this.input.val().trim()) {
+			return;
+		}
+		
+		app.todoList.create({ title : this.input.val().trim() });
+		this.input.val('');
+	},
+	
+	add : function(item) {
+		var view = new app.TodoItemView({ model : todo });
+		this.$el.append(view.render().el);
+	},
+	
+	reset : function() {
+		this.$el.html('');
+		app.todoList.each(this.addOne, this);
 	},
 	
 	render : function() {
 		this.$el.html("Hello world");
+		
 	}
 });
